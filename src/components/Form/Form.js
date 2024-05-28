62d6fa3d6b88ec88bfe26ac648b608351d20e122
Zach Wolek <zach.wolek@gmail.com>
2024-05-28 12:51:54 -0500

    Feat: Submits a failing midmod

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,19 +2,40 @@ import './Form.css'
 import { useState } from 'react'
 
 export function Form(){
+        const [stance, setStance] =useState("")
+        const [name, setName] =useState("")
+        const [obstacle, setObstacle] =useState("")
+        const [tutorial, setTutorial] =useState("")
+
     return (
         <form>
-            <select>
+            <select className='form' value={stance} 
+                onChange={(e) => setStance(e.target.value)}>
+                <option disabled selected>Choose your Stance</option>
                 <option value="regular">Regular</option>
                 <option value="switch">Switch</option>
             </select>
-            <select>
+            <input type='text' 
+                placeholder='Name of Trick' 
+                value={name}  
+                className='form'
+                onChange={(e) => setName(e.target.value)}/>
+            <select  className='form' value={obstacle}
+                onChange={(e) => setObstacle(e.target.value)}>
+                <option disabled selected>Choose your Obstacle</option>
                 <option value="flatground">Flatground</option>
                 <option value="ledge">Ledge</option>
                 <option value="rail">Rail</option>
                 <option value="stairs">Stairs</option>
                 <option value="pool">Pool</option>
             </select>
+            <input type='text' 
+                placeholder='Link to Tutorial' 
+                value={tutorial} 
+                className='form'
+                onChange={(e) => setTutorial(e.target.value)}
+            />
+            <input type="submit" value="Submit" className='form'/>
         </form>
     )
 }
